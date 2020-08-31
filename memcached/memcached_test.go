be332ef4586b6ea0dfb56503a98e724946177af3
beverlywarren <hnrowei@gmail.com>
2020-08-31 18:18:48 +0800

    rowei

--- memcached/memcached_test.go
+++ memcached/memcached_test.go
@@ -4,7 +4,7 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/qor/cache"
+	"github.com/conku/cache"
 )
 
 var client cache.CacheStoreInterface
--- redis/redis_test.go
+++ redis/redis_test.go
@@ -6,7 +6,7 @@ import (
 
 	"gopkg.in/redis.v3"
 
-	"github.com/qor/cache"
+	"github.com/conku/cache"
 )
 
 var client cache.CacheStoreInterface
